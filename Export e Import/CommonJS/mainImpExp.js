67656bb8a1b6d4fba560dbe7ac4e75a686e5f4b2
Plussbyte <samuel.estrada139@pascualbravo.edu.co>
2024-06-01 07:02:58 -0500

    Node.js A1

---
+++ Export e Import/CommonJS/mainImpExp.js
@@ -0,0 +1,8 @@
+//CommonJS require module       
+const {sum} = require('./sum')  
+console.log(sum(1, 3))          
+
+
+//.js utiliza commonJS por defecto
+//.mjs utiliza ES Modules
+//.cjs utiliza commonJS
---
+++ Modulos nativos/modules.js
@@ -0,0 +1,5 @@
+const modules = require('node:os')
+
+console.log('Informacion')
+
+console.log('Nombre del sistema operativo', modules.platform())
